1b1742052cab3581065937e95f8b0398f0eb3764
Morgan Houppin <morganhouppin@gmail.com>
2026-07-29 09:39:38 +0200

    Reformatting + minor code style changes
    Bench: 4532959

--- src/sources/search.c
+++ src/sources/search.c
@@ -669,19 +669,20 @@ Score search(
 
     // Razoring. If our static eval isn't good, and depth is low, it is likely that only a capture
     // will save us at this stage. Drop into qsearch.
-    const int razoring_margin = 120 * depth;
-    if (!pv_node && !ss->excluded_move && !in_check && ss->static_eval + razoring_margin <= alpha && depth <= 3) {
-        const bool allow_full_razor = depth == 1 ||
-            (depth <= 2 && ss->static_eval + razoring_margin <= alpha);
+    const Score razoring_margin = 120 * depth;
+
+    if (!pv_node && !ss->excluded_move && !in_check && ss->static_eval + razoring_margin <= alpha
+        && depth <= 3) {
+        const bool allow_full_razor =
+            depth == 1 || (depth <= 2 && ss->static_eval + razoring_margin <= alpha);
 
         if (allow_full_razor) {
             return qsearch(false, board, alpha, beta, ss);
         }
 
-        const int capped_alpha = i16_max(alpha - razoring_margin, -MATE);
-        const int razor_alpha = capped_alpha;
-        const int razor_beta = razor_alpha + 1;
-        int razor_score = qsearch(false, board, razor_alpha, razor_beta, ss);
+        const Score capped_alpha = i16_max(alpha - razoring_margin, -MATE);
+        const Score razor_alpha = capped_alpha;
+        Score razor_score = qsearch(false, board, razor_alpha, razor_alpha + 1, ss);
 
         // We proved a fail-low, so we can return the score.
         if (razor_score <= razor_alpha) {
@@ -689,7 +690,7 @@ Score search(
         }
 
         if (razor_score >= beta + 120 && depth <= 3) {
-            depth -= i16_min(1, depth - 1);
+            depth = i16_max(1, depth - 1);
         }
     }
 
